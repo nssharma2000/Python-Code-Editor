1c75319dd9443ddaa364c15877f89ea2d4b7f4cd
nssharma2000 <140267226+nssharma2000@users.noreply.github.com>
2025-05-31 20:21:04 +0530

    First commit

---
+++ backend/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import subprocess
+import tempfile
+import os
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/run", methods=["POST"])
+def run_code():
+    data = request.get_json()
+    code = data.get("code")
+    stdin = data.get("input", "")
+
+    if stdin.strip() == "":
+        stdin = "\n"
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as temp_file:
+        temp_file.write(code)
+        temp_file_path = temp_file.name
+
+    try:
+        result = subprocess.run(["python", temp_file_path], input=stdin.encode(), capture_output=True, timeout=5)
+        output = result.stdout.decode() + result.stderr.decode()
+    except subprocess.TimeoutExpired:
+        output = "Error: Execution timed out."
+    except Exception as e:
+        output = f"Error: {str(e)}"
+    finally:
+        os.remove(temp_file_path)
+
+    return jsonify({"output": output})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Editor from '@monaco-editor/react';
+import axios from 'axios';
+
+function App() {
+
+  let backendUrl = "http://127.0.0.1:5000"
+
+  const [code, setCode] = useState("#Type code here")
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+
+  async function handleRun () {
+    setOutput("Running...")
+    try {
+      const res = await axios.post(`${backendUrl}/run`, { code , input })
+      setOutput(res.data.output)
+    } catch (err) {
+      setOutput("Error: " + err.message)
+    }
+  }
+
+  return (
+    <div className="p-4 bg-slate-100 text-center mx-auto w-[100%]">
+      <h1 className="text-2xl font-bold">Mini Coding Editor</h1>
+      <Editor
+        height="300px" language="python" defaultValue={code} theme="vs-dark" onChange={(value) => setCode(value)} />
+      
+      <textarea className="mt-4 w-full p-2 border bg-neutral-100 rounded" placeholder="Enter input here" rows={4} value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleRun} className="px-4 my-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+        Run
+      </button>
+      <pre className="bg-black h-[20vh] text-green-400 p-4 rounded whitespace-pre-wrap">
+        {output}
+      </pre>
+    </div>
+  );
+}
+
+export default App
+
